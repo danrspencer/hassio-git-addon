208d7f4324de85fdfe585f33e1dffdc674cc771d
Dan Spencer <dan.spencer@anaplan.com>
2019-08-18 20:18:34 +0100

    Can checkout git remote

--- hassio-git/run.py
+++ hassio-git/run.py
@@ -3,33 +3,65 @@
 import json
 import os
 import yaml
+import sys
+
+import subprocess
+from subprocess import call, check_output
 
 config_path = "/config"
 options_path = "/data/options.json"
 secrets_path = "/config/secrets.yaml"
 
 ssh_key_path = "/root/.ssh/id_rsa"
+known_hosts_path = "/root/.ssh/known_hosts"
 
-os.chdir(config_path)
 
-with open(options_path, 'r') as stream:
-    options_json = stream.read()
-    options = json.loads(options_json)
+def load_options():
+    with open(options_path, 'r') as stream:
+        options_json = stream.read()
+        options = json.loads(options_json)
 
-with open(secrets_path, 'r') as stream:
-    try:
-        secrets = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
+    return options
 
-with open(ssh_key_path, 'w') as stream:
-    stream.write(secrets["git_ssh"])
+
+def setup_ssh():
+    with open(secrets_path, 'r') as stream:
+        try:
+            secrets = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    with open(ssh_key_path, 'w', encoding='utf-8') as stream:
+        stream.write(secrets["git_ssh"])
+
     os.chmod(ssh_key_path, 0o600)
 
-print(secrets)
+    known_hosts = check_output(
+        ["ssh-keyscan", "github.com"]).decode(sys.stdout.encoding)
+
+    with open(known_hosts_path, 'w', encoding='utf-8') as stream:
+        stream.write(known_hosts)
 
 
-# git_status = os.popen("git status --porcelain").read()
+def clone_remote(repository):
+    os.chdir(config_path)
 
-# if len(git_status) > 0:
-#     os.system("git commit -m 'Auto commit from hassio-git...'; git push")
+    try:
+        remotes = check_output(["git", "remote", "-v"]
+                               ).decode(sys.stdout.encoding)
+    except:
+        remotes = ""
+
+    if repository not in remotes:
+        print("Checking out {}".format(repository))
+        call(["git", "init"])
+        call(["git", "remote", "add", "origin", repository])
+        call(["git", "pull", "origin", "master"])
+    else:
+        print("Repository already checked out.")
+
+
+options = load_options()
+setup_ssh()
+
+clone_remote(options['repository'])
